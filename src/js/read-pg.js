4b13d826eefac05b958ca1548f1132b4c772c48a
albuska <109683293+albuska@users.noreply.github.com>
2023-03-02 19:29:49 +0200

    add active button

--- src/js/read-pg.js
+++ src/js/read-pg.js
@@ -1,9 +1,12 @@
 
 const dropBtnRef = document.getElementById('dropBtn-js'); 
 const dropIcon = document.querySelector('.icon-down-read-pg'); 
+const newsListRef = document.querySelector('.news-list'); 
 
 dropBtnRef.addEventListener('click', function () {
-    // filterItems.classList.toggle('visually-hidden');
+    newsListRef.classList.toggle('show');
     dropIcon.classList.toggle('rotate');
   });
 //   window.addEventListener('resize', debounce(restart, 250));
+
+ 
