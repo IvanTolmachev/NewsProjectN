950e04dd2b703b34e941154345f3ae7d439688ea
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-03-03 14:15:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import './js/filter-categories';
 import './js/home-page';
 import './js/weather';
 import './js/toggle-mode';
-// import './js/add-to-favorite';
-// import './js/search_cards.js';
+import './js/add-to-favorite';
+import './js/search_cards.js';
