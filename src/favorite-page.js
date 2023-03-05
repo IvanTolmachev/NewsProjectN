85328551541da21b8f6d429dd51b71c383210361
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-03-05 17:43:00 +0200

    Card update

--- src/favorite-page.js
+++ src/favorite-page.js
@@ -1,4 +1,4 @@
 import './js/toggle-mode.js';
 import './js/createMarkupFavorite.js';
-import './js/add-to-favorite.js';
-import './js/remove-from-favorite.js';
+import './js/remove-from-favorite';
+
