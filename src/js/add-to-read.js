5dd0972db31a1620dfa35b50a8565cc674eb65fd
albuska <109683293+albuska@users.noreply.github.com>
2023-03-05 22:24:41 +0200

    add sort

--- src/js/add-to-read.js
+++ src/js/add-to-read.js
@@ -1,55 +1,11 @@
-// import { savedApiData } from './read'; 
 
-// const STORAGE_KEY = 'readNews';
- 
-// const readNews = [];
-// const gallery = document.querySelector('.gallery'); 
-
-
-// gallery.addEventListener('click', getReadNewsId);
-
-// export default function getReadNewsId(event) {
-
-//   //  const readDate = new Date().toLocaleDateString(); 
-//   //  console.log("🚀 ~ getReadNewsId ~ date:", readDate); 
-
-//   if(event.target.classList.contains("wrap-info__link")){ 
-//     spanBtnRef = new Date().toLocaleDateString(); 
-//     event.preventDefault();
-
-//     const id=event.target.closest(".js-card-item").dataset.targetId
-
-//     saveReadNew(id);
-//   }
-// }
-
-// function saveReadNew(id) {
-
-//   const readNew = savedApiData.find(item=>item.id===id)
-
-//   if (readNews.length<1){
-//     readNews.push(readNew); 
-// } 
-
-// if(readNews.every(el=>Number(el.id)!==Number(id))) {
-//   readNews.push(readNew); 
-// }
-// console.log(readNews); 
-// localStorage.setItem(STORAGE_KEY, JSON.stringify(readNews));
-// }
-  // *******************************
-  // import axios from 'axios';
-
-  // import { savedApiData } from './read'; 
-// import { getCards } from './cards';
-// import { createCards } from './cards';
 import { savedApiData } from './cards'; 
 
   const STORAGE_KEY = 'readNews';
    
   let readNews = [];
  
-
+  
   const gallery = document.querySelector('.gallery'); 
   
   gallery.addEventListener('click', getReadNewsId);
@@ -86,10 +42,6 @@ import { savedApiData } from './cards';
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(readNews));
 
-    if(localStorage === '') {
-      errorRequest.classList.remove('visually-hidden');   
-    }
   } 
   } 
 // // ***********************************
-
--- src/js/createMarkupRead.js
+++ src/js/createMarkupRead.js
@@ -1,12 +1,12 @@
 
-import { createCards } from './cards';
+import { createMarkup } from './cards';
 
 
 //  const btnMoreRead = document.querySelector('.dropbtn'); 
 const dropIcon = document.querySelector('.icon-down-read-pg'); 
 const newsListRef = document.querySelector('.news-list'); 
 const dateListRef = document.querySelector('.date-list');
-// console.log("🚀 ~ newsListRef:", newsListRef);
+const errorRequest = document.querySelector('.errorRequest');
 
 
 dateListRef.addEventListener('click', function () {
@@ -29,90 +29,29 @@ export default function createMarkupRead() {
     let markup = "";
     
     markup = storageData
-    .map(({ id, url, title, section, abstract, published_date, media }) => {
-      let imgUrl = media.map(media => media['media-metadata'][2].url);
-      let newDateStr = published_date.replace(/-/g, '/');
-      //Проверка есть ли эта новость в Favorite
-      // checkIsNewFavorite(id)
-      // console.log("🚀 ~ storageNews:", storageNews)
-      if (
-        Boolean(storageNews) &
-        storageNews.some(el => Number(el.id) === Number(id))
-      ) {
-        // console.log(" Перевірка! Есть favorite новости")
-        return `<li class="card js-card-item" data-target-id=${id}>
-        <div class="wrap-image">
-          <img
-            src="${imgUrl}"
-            alt="photo"
-            class="wrap-image__photo"
-          />
-          <p class="wrap-image__text">${section}</p>
-          <button type="button"  class="wrap-image__btn js-is-favorite">
-          <span class="wrap-image__btn-text js-is-favorite ">Remove from favorite</span>
-            <svg class="wrap-image__icon js-is-favorite fill-heard" width="16" height="16">
-                <use class="js-is-favorite" href ='${iconHeart}#icon-heart'></use>
-            </svg>
-          </button>
-        
-        </div>
-            <h2 class="card__title">${title}</h2>
-            <p class="card__description">${
-              abstract.length > 112 ? abstract.slice(0, 113) + '...' : abstract
-            }</p>
-            <div class="wrap-info">
-                <p class="wrap-info__time">${newDateStr}</p>
-                <a href="${url}" class="wrap-info__link">Read more</a>
-            </div>
-        </li>`;
-      }
-      //
-      else
-        return `
-         <li class="card  js-card-item" data-target-id="${id}">
-      <div class="wrap-image">
-          <img
-            src="${imgUrl}"
-            alt="photo"
-           class="wrap-image__photo"
-          />
-          <p class="wrap-image__text">${section}</p>
-          <button type="button" class="wrap-image__btn js-tartet-favorite">
-          <span class="wrap-image__btn-text js-tartet-favorite">Add to favorite</span>
-           <svg class="wrap-image__icon js-tartet-favorite" width="16" height="16">
-                <use href ='${iconHeart}#icon-heart' class="js-tartet-favorite"></use>
-              </svg></button>
-        </div>
-        <h2 class="card__title">${title}</h2>
-        <p class="card__description">${
-          abstract.length > 112 ? abstract.slice(0, 113) + '...' : abstract
-        }</p>
-        <div class="wrap-info">
-          <p class="wrap-info__time">${newDateStr}</p>
-          <a href="${url}" class="wrap-info__link" target="_blank" rel="noreferrer noopener">Read more</a>
-        </div>
-      </li>
-     `;
-    })
+    .map(createMarkup)
       .join("");
       // console.log("🚀 ~ createMarkupFavorite ~ markup:", markup)
     newsListRef.insertAdjacentHTML("beforeend", markup);
-
+    // if(newsListRef === '') {
+    //   errorRequest.classList.add('visually-hidden');   
+    // }
+  readDateCard(storageData.readDateNew); 
   }
   
 
 
-  // function readDateCard(items) {
-  //   console.log(items); 
-   
-//   dateMarkup = `<li class="dropbtn calendar-btn-span">
-//   <span class="btn-span">${date}</span>
-//     <svg class="icon-down-read-pg" width="15" height="9">
-//         <use href="/src/icon.svg#icon-arrow-down"></use>
-//       </svg>
-// </li>`
-// return dateMarkup; 
-// }
-// dateListRef.insertAdjacentHTML('beforeend', dateMarkup);
+  function readDateCard(items) {
+    console.log(items); 
+    const markup = items.map(({ readDateNew }) => {
+      return `<li class="dropbtn calendar-btn-span">
+      <span class="btn-span">${readDateNew}</span>
+        <svg class="icon-down-read-pg" width="15" height="9">
+            <use href="/src/icon.svg#icon-arrow-down"></use>
+          </svg>
+  </li>`
+  }).sort((a, b) => b - a).join(""); 
+  dateListRef.insertAdjacentHTML('beforeend', markup); 
+}
 
 
