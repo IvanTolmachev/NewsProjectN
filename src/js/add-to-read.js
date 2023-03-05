8739192af8a1c6f534cfd5bd5fd16f484b0bf3be
albuska <109683293+albuska@users.noreply.github.com>
2023-03-05 22:35:24 +0200

    Update add-to-read.js

--- src/js/add-to-read.js
+++ src/js/add-to-read.js
@@ -5,7 +5,6 @@ import { savedApiData } from './cards';
    
   let readNews = [];
  
-  
   const gallery = document.querySelector('.gallery'); 
   
   gallery.addEventListener('click', getReadNewsId);
@@ -16,21 +15,15 @@ import { savedApiData } from './cards';
     //  console.log("🚀 date:", readDate); 
   
     if(event.target.classList.contains("wrap-info__link")){ 
-    
-      event.currentTarget.children.style.opacity = '40%';
-
       event.preventDefault();
   
       const id=event.target.closest(".js-card-item").dataset.targetId
-  
       saveReadNew(id);
     }
   }
   
   export function saveReadNew(id) {
-
     const readNew = savedApiData.find(item=>item.id===id)
- 
     readNew.readDateNew = readDate; 
 
     if (readNews.length<0){
