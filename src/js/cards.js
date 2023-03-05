4ff0d05ef11fe9e6c0093b175185a9567336e06b
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-03-05 23:06:21 +0200

    Update cards.js

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
-// import { KEY } from './api-key';
-const API_KEY = 'VPd8ESOXXGRNi6SUHc4QYJMXdqmRVK3K';
+import { KEY } from './api-key';
 
 export async function getCards() {
-  const URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`;
+  const URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${KEY}`;
   const requestData = await axios.get(URL);
   return requestData;
 }
-const STORAGE_KEY = 'favoriteNews';
-const storageNews = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
 const iconHeart = new URL('../images/icon.svg', import.meta.url);
 
 const refs = {
@@ -35,44 +33,8 @@ export function createMarkup(arr) {
     .map(({ id, url, title, section, abstract, published_date, media }) => {
       let imgUrl = media.map(media => media['media-metadata'][2].url);
       let newDateStr = published_date.replace(/-/g, '/');
-      //Проверка есть ли эта новость в Favorite
-      // checkIsNewFavorite(id)
-      // console.log("🚀 ~ storageNews:", storageNews)
-      if (
-        Boolean(storageNews) &
-        storageNews.some(el => Number(el.id) === Number(id))
-      ) {
-        // console.log(" Перевірка! Есть favorite новости")
-        return `<li class="card js-card-item" data-target-id=${id}>
-        <div class="wrap-image">
-          <img
-            src="${imgUrl}"
-            alt="photo"
-            class="wrap-image__photo"
-          />
-          <p class="wrap-image__active" hidden>Already read</p>
-          <p class="wrap-image__text">${section}</p>
-          <button type="button"  class="wrap-image__btn js-is-favorite">
-          <span class="wrap-image__btn-text js-is-favorite ">Remove from favorite</span>
-            <svg class="js-is-favorite fill-heard" width="16" height="16">
-                <use class="js-is-favorite" href ='${iconHeart}#icon-heart'></use>
-            </svg>
-          </button>
-        
-        </div>
-            <h2 class="card__title">${title}</h2>
-            <p class="card__description">${
-              abstract.length > 112 ? abstract.slice(0, 113) + '...' : abstract
-            }</p>
-            <div class="wrap-info">
-                <p class="wrap-info__time">${newDateStr}</p>
-                <a href="${url}" class="wrap-info__link">Read more</a>
-            </div>
-        </li>`;
-      }
-      //
-      else
-        return `
+
+      return `
          <li class="card  js-card-item" data-target-id="${id}">
       <div class="wrap-image">
           <img
@@ -106,13 +68,12 @@ function saveApiData(arrey) {
   arrey.map(({ id, url, title, section, abstract, published_date, media }) => {
     const item = {};
     let imgUrl = media.map(media => media['media-metadata'][2].url);
-    let newDateStr = published_date.replace(/-/g, '/');
     item['id'] = `${id}`;
     item['url'] = `${url}`;
     item['title'] = `${title}`;
     item['section'] = `${section}`;
     item['abstract'] = `${abstract}`;
-    item['newDateStr'] = `${newDateStr}`;
+    item['published_date'] = `${published_date}`;
     item['imgUrl'] = `${imgUrl}`;
     savedApiData.push(item);
   });
