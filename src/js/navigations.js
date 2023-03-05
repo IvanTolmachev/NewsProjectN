35fa9d8aa546d1aa4f699ed4c297e3a26f0033f9
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-03-05 16:46:19 +0200

    up

--- src/js/navigations.js
+++ src/js/navigations.js
@@ -3,10 +3,10 @@ const home = document.getElementById('home-link');
 const favorite = document.getElementById('favorite-link');
 const read = document.getElementById('read-link');
 
-if (currentPage === '/index.html') {
+if (currentPage === '../index.html') {
   home.classList.add('current');
-} else if (currentPage === '/favorite.html') {
+} else if (currentPage === '../favorite.html') {
   favorite.classList.add('current');
-} else if (currentPage === '/read.html') {
+} else if (currentPage === '../read.html') {
   read.classList.add('current');
 }
