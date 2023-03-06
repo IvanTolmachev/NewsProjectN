a832a3af71aec067585c721f6f43422d4fee24a2
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-03-06 06:00:17 +0200

    Update navigations.js

--- src/js/navigations.js
+++ src/js/navigations.js
@@ -2,16 +2,12 @@ const currentPage = window.location.pathname;
 const home = document.getElementById('home-link');
 const favorite = document.getElementById('favorite-link');
 const read = document.getElementById('read-link');
-// console.log(window.location);
-// const iconHeart = new URL('../../src/*.html', import.meta.url);
-// import wIcon from '../../src/index.html';
-console.log(window.location.href.toString().split(window.location.origin)[0]);
-// console.log(wIcon);
-if (currentPage === '/index.html') {
+
+if (currentPage.includes('/index.html')) {
   home.classList.add('current');
-} else if (currentPage === '/favorite.html') {
+} else if (currentPage.includes('/favorite.html')) {
   favorite.classList.add('current');
-} else if (currentPage === '/read.html') {
+} else if (currentPage.includes('/read.html')) {
   read.classList.add('current');
 }
 console.log(currentPage);
