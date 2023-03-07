1e1e2846964ca58f88d0135faacdd129da3fc004
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-03-08 01:40:15 +0200

    Update createMarkupRead.js

--- src/js/createMarkupRead.js
+++ src/js/createMarkupRead.js
@@ -1,33 +1,39 @@
-
 const iconHeart = new URL('../images/icon.svg', import.meta.url);
- const btnMoreRead = document.querySelector('.dropbtn'); 
-const dropIcon = document.querySelector('.icon-down-read-pg'); 
+const btnMoreRead = document.querySelector('.dropbtn');
+const dropIcon = document.querySelector('.icon-down-read-pg');
 // const dateListRef = document.querySelector('.date-list');
 const errorRequest = document.querySelector('.errorRequest');
-const itemListRef = document.querySelector('.item-list'); 
-
-
+const itemListRef = document.querySelector('.item-list');
 
 const STORAGE_KEY = 'readNews';
 const STORAGE_KEY_FAVORITE = 'favoriteNews';
 // ++++++++++++++++++++++++
 
 const storageJson = localStorage.getItem(STORAGE_KEY);
-    console.log("🚀 ~ createMarkupRead ~ storageJson:", storageJson)
-    const storageData = JSON.parse(storageJson); 
-    console.log("🚀 ~ createMarkupRead ~ storageData:", storageData); 
+console.log('🚀 ~ createMarkupRead ~ storageJson:', storageJson);
+const storageData = JSON.parse(storageJson);
+console.log('🚀 ~ createMarkupRead ~ storageData:', storageData);
 const favoriteNews = JSON.parse(localStorage.getItem(STORAGE_KEY_FAVORITE));
-   console.log("🚀 ~ Boolean(favoriteNews)", Boolean(favoriteNews))
-   
+console.log('🚀 ~ Boolean(favoriteNews)', Boolean(favoriteNews));
 
 export function createMarkupRead(arr) {
-      if (!Boolean(storageData)) {
-        errorRequest.classList.remove('visually-hidden'); 
-        return; 
-      }
+  if (!Boolean(storageData)) {
+    errorRequest.classList.remove('visually-hidden');
+    return;
+  }
 
-      const markup = arr
-      .map(({ id, url, title, section, abstract, published_date, media, imgUrl }) => {
+  const markup = arr
+    .map(
+      ({
+        id,
+        url,
+        title,
+        section,
+        abstract,
+        published_date,
+        media,
+        imgUrl,
+      }) => {
         // let imgUrl = media.map(media => media['media-metadata'][2].url);
         let newDateStr = published_date;
         //Проверка есть ли эта новость в Favorite
@@ -56,7 +62,9 @@ export function createMarkupRead(arr) {
           </div>
               <h2 class="card__title">${title}</h2>
               <p class="card__description">${
-                abstract.length > 112 ? abstract.slice(0, 113) + '...' : abstract
+                abstract.length > 112
+                  ? abstract.slice(0, 113) + '...'
+                  : abstract
               }</p>
                   <p class="wrap-info__time">${newDateStr}</p>
                   <a href="${url}" class="wrap-info__link">Read more</a>
@@ -89,32 +97,35 @@ export function createMarkupRead(arr) {
             <p class="wrap-image__active visually-hidden">Already read</p>
         </li>
        `;
-      })
-      .join('');
-    itemListRef.insertAdjacentHTML('beforeend', markup);
-  }
-   
-  
-    createMarkupRead(storageData);
- 
-  btnMoreRead.addEventListener('click', function () {
-    itemListRef.classList.toggle('show');
-    dropIcon.classList.toggle('rotate');
-  });
+      }
+    )
+    .join('');
+  itemListRef.insertAdjacentHTML('beforeend', markup);
+}
 
-readDateCard(storageData); 
+createMarkupRead(storageData);
 
-  function readDateCard(items) {
-    console.log(items); 
-    const markup = items.map(({ readDateNew }) => {
+btnMoreRead.addEventListener('click', function () {
+  itemListRef.classList.toggle('show');
+  dropIcon.classList.toggle('rotate');
+});
+
+readDateCard(storageData);
+
+function readDateCard(items) {
+  console.log(items);
+  const markup = items
+    .map(({ readDateNew }) => {
       return `<li>
       <span class="btn-span">${readDateNew}</span>
         <svg class="icon-down-read-pg" width="15" height="9">
-            <use href="/src/icon.svg#icon-arrow-down"></use>
+            <use href="${iconHeart}#icon-arrow-down"></use>
           </svg>
-  </li>`
-  }).sort((a, b) => b.readDateNew - a.readDateNew).join(""); 
-  btnMoreRead.insertAdjacentHTML('beforeend', markup); 
+  </li>`;
+    })
+    .sort((a, b) => b.readDateNew - a.readDateNew)
+    .join('');
+  btnMoreRead.insertAdjacentHTML('beforeend', markup);
 }
 
 // items.sort((a, b) => new Date(b.readDateNew) - new Date(a.readDateNew));
