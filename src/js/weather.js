bccacd19e7a0c5434f3e6f1bf4bf23b992666380
Margaryta Myagkova <margomyagkova@gmail.com>
2023-03-07 19:43:21 +0200

    Added week date

--- src/js/weather.js
+++ src/js/weather.js
@@ -77,6 +77,7 @@ function errorWeekWeatherCallback() {
         const weatherDay = weatherData[i];
 
         const date = new Date(weatherDay.dt * 1000);
+        const dayOfMonth = date.getDate();
         const dayOfWeek = daysOfWeek[date.getDay()];
 
         const iconCode = weatherDay.weather[0].icon;
@@ -90,7 +91,7 @@ function errorWeekWeatherCallback() {
 
         const weatherDate = document.createElement('p');
         weatherDate.classList.add('weather__week-date');
-        weatherDate.textContent = dayOfWeek;
+        weatherDate.textContent = `${dayOfMonth} ${dayOfWeek}`;
 
         const weatherIcon = document.createElement('img');
         weatherIcon.classList.add('weather__week-icon');
@@ -140,6 +141,7 @@ function getWeatherForWeek(position) {
         const weatherDay = weatherData[i];
 
         const date = new Date(weatherDay.dt * 1000);
+        const dayOfMonth = date.getDate();
         const dayOfWeek = daysOfWeek[date.getDay()];
 
         const iconCode = weatherDay.weather[0].icon;
@@ -153,7 +155,7 @@ function getWeatherForWeek(position) {
 
         const weatherDate = document.createElement('p');
         weatherDate.classList.add('weather__week-date');
-        weatherDate.textContent = dayOfWeek;
+        weatherDate.textContent = `${dayOfMonth} ${dayOfWeek}`;
 
         const weatherIcon = document.createElement('img');
         weatherIcon.classList.add('weather__week-icon');
