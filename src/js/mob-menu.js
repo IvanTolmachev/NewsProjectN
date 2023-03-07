e57eac16e6ee4d5b5f0c8c5107b5f1153990cd49
AndriySafonov <andreygold18@gmail.com>
2023-03-07 12:23:56 +0200

    renamed classes wrote javascript code for current

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,18 +1,37 @@
 
 
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.mobile__menu-btn');
+const menu = document.querySelector('.mobile__menu');
 const bodyScroll = document.querySelector('body');
 
 menuBtn.addEventListener('click', function () {
   const expanded = menuBtn.getAttribute('aria-expanded') === 'true' || false;
 
-  menuBtn.classList.toggle('inActions');
+  menuBtn.classList.toggle('isMenuOpen');
   menuBtn.setAttribute('aria-expanded', !expanded);
 
-  menu.classList.toggle('inActions');
+  menu.classList.toggle('isMenuOpen');
   bodyScroll.classList.toggle('body-scroll');
 });
+
+// -------------------------------------------------------
+
+//  получаем текущий путь страницы с помощью 
+//  window.location.pathname и затем проверяем 
+//  каждую ссылку на соответствие этому пути.
+//  Если ссылка соответствует текущей странице, 
+//  мы добавляем класс.active к родительскому элементу.
+
+const links = document.querySelectorAll('.mobile__menu-link');
+const currentPath = window.location.pathname;
+
+links.forEach(link => {
+  if (link.pathname === currentPath) {
+    link.parentElement.classList.add('active');
+  }
+});
+console.log(currentPath);
+
 
 // ------------------------------------------------------
 // const currentPage = window.location.pathname;
@@ -28,3 +47,6 @@ menuBtn.addEventListener('click', function () {
 //   read.classList.add('current');
 // }
 // console.log(currentPage);
+
+// -----------------------------------------------------------
+
